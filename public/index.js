50a6ee49e73c65a6fca5d9d9627a96f30bda060b
farva9999 <decofinde@gmail.com>
2023-08-01 14:13:08 -0400

    d

--- public/index.js
+++ public/index.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "/__/firebase/10.1.0/firebase-app-compat.js";
+import { getAnalytics } from "/__/firebase/10.1.0/firebase-analytics-compat.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
